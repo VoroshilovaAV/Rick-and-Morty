8792969a44fc0750d490f5a4079c78ee255b7123
VoroshilovaAV <Voroshilova.AV@yandex.ru>
2022-04-02 13:43:02 +0400

    refactor: change component NotFound to functional

--- react-components/src/pages/NotFound/NotFound.tsx
+++ react-components/src/pages/NotFound/NotFound.tsx
@@ -1,9 +1,3 @@
-import React from 'react';
-
-class NotFound extends React.Component {
-  render() {
-    return <h1 data-testid="error-page">404 Not Found</h1>;
-  }
+export default function NotFound() {
+  return <h1 data-testid="error-page">404 Not Found</h1>;
 }
-
-export default NotFound;
