0a94b8efd9cdc8950b7995dd44b60de7a78f9450
VoroshilovaAV <Voroshilova.AV@yandex.ru>
2022-04-08 12:10:30 +0400

    refactor: optimize ErrorMessage component code

--- react-forms/src/pages/Forms/components/error/ErrorMessage.tsx
+++ react-forms/src/pages/Forms/components/error/ErrorMessage.tsx
@@ -1,23 +1,9 @@
 export default function ErrorMessage(props: { validateData: Array<string>; input: string }) {
-  if (props.input == 'agree') {
-    return (
-      <>
-        {props.validateData.includes(props.input) ? (
-          <p className="forms__name_error">click here</p>
-        ) : (
-          <p className="forms__name_hide">hide</p>
-        )}
-      </>
-    );
-  } else {
-    return (
-      <>
-        {props.validateData.includes(props.input) ? (
-          <p className="forms__name_error">enter a valid {props.input}</p>
-        ) : (
-          <p className="forms__name_hide">hide</p>
-        )}
-      </>
-    );
-  }
+  return props.validateData.includes(props.input) ? (
+    <p className="forms__name_error">
+      {props.input == 'agree' ? 'click here' : `enter a valid ${props.input}`}
+    </p>
+  ) : (
+    <p className="forms__name_hide">hide</p>
+  );
 }
