16474dd6980ebf3457bb4139096140d1e5665ab9
VoroshilovaAV <Voroshilova.AV@yandex.ru>
2022-04-08 11:49:51 +0400

    refactor: rewrite Subscriber and ErrorMessage components to functional

--- react-forms/src/pages/Forms/components/error/ErrorMessage.tsx
+++ react-forms/src/pages/Forms/components/error/ErrorMessage.tsx
@@ -1,34 +1,23 @@
-import React from 'react';
-
-type Props = {
-  validateData: Array<string>;
-  input: string;
-};
-
-class ErrorMessage extends React.Component<Props> {
-  render() {
-    if (this.props.input == 'agree') {
-      return (
-        <>
-          {this.props.validateData.includes(this.props.input) ? (
-            <p className="forms__name_error">click here</p>
-          ) : (
-            <p className="forms__name_hide">hide</p>
-          )}
-        </>
-      );
-    } else {
-      return (
-        <>
-          {this.props.validateData.includes(this.props.input) ? (
-            <p className="forms__name_error">enter a valid {this.props.input}</p>
-          ) : (
-            <p className="forms__name_hide">hide</p>
-          )}
-        </>
-      );
-    }
+export default function ErrorMessage(props: { validateData: Array<string>; input: string }) {
+  if (props.input == 'agree') {
+    return (
+      <>
+        {props.validateData.includes(props.input) ? (
+          <p className="forms__name_error">click here</p>
+        ) : (
+          <p className="forms__name_hide">hide</p>
+        )}
+      </>
+    );
+  } else {
+    return (
+      <>
+        {props.validateData.includes(props.input) ? (
+          <p className="forms__name_error">enter a valid {props.input}</p>
+        ) : (
+          <p className="forms__name_hide">hide</p>
+        )}
+      </>
+    );
   }
 }
-
-export default ErrorMessage;
--- react-forms/src/pages/Forms/components/subscriber/Subscriber.tsx
+++ react-forms/src/pages/Forms/components/subscriber/Subscriber.tsx
@@ -1,30 +1,23 @@
-import React from 'react';
 import './index.scss';
 import { FormState } from '../../Forms';
 
-type Props = { item: FormState; key: number };
-
-class Subscriber extends React.Component<Props> {
-  render() {
-    return (
-      <>
-        <div className="wrapper">
-          <div className="subscriber">
-            <img className="subscriber__img" src={this.props.item.file} alt="avatar" />
-            <div className="subscriber__info">
-              <h4 className="subscriber__name">
-                {this.props.item.name} {this.props.item.surname}
-              </h4>
-              <hr />
-              <p className="subscriber__text">Date of Birth: {this.props.item.date}</p>
-              <p className="subscriber__text">Country: {this.props.item.country}</p>
-              <p className="subscriber__text">Gender: {this.props.item.gender}</p>
-            </div>
+export default function Subscriber(props: { item: FormState }) {
+  return (
+    <>
+      <div className="wrapper">
+        <div className="subscriber">
+          <img className="subscriber__img" src={props.item.file} alt="avatar" />
+          <div className="subscriber__info">
+            <h4 className="subscriber__name">
+              {props.item.name} {props.item.surname}
+            </h4>
+            <hr />
+            <p className="subscriber__text">Date of Birth: {props.item.date}</p>
+            <p className="subscriber__text">Country: {props.item.country}</p>
+            <p className="subscriber__text">Gender: {props.item.gender}</p>
           </div>
         </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
 }
-
-export default Subscriber;
